618c572eea57f2c1e7c092f153bec732d1f73cec
HP <HP@DESKTOP-4Q8V1JF>
2023-06-08 21:33:35 +0530

    Add project files.

---
+++ Arithmatic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+    internal class Arithmatic
+    {
+        public void findMaxinumberArray()
+        {
+            int[] num= new int[10];
+            Console.WriteLine("please enter 10 numbers");
+            for (int i = 0;i<10;i++)
+            {
+                num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int max = num[0];
+            for(int i = 1;i<10; i++)
+            {
+                if (max < num[i])
+                {
+                    max = num[i];
+                }
+            }
+            Console.WriteLine("The maximumnum= + max");
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ArrayPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Arithmatic arithmatic = new Arithmatic();
+            arithmatic.findMaxinumberArray();
+        }
+    }
+}
